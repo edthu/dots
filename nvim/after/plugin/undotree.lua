9679634d546197bf6f5fdd8b2a4bc32b8826e526
edthu <eetu.jalava@hotmail.fi>
2023-11-18 23:58:10 +0200

    nvim

---
+++ nvim/after/plugin/undotree.lua
@@ -0,0 +1,10 @@
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+vim.cmd [[
+hi UndotreeNormal guibg=NONE
+]]
+
+-- Make undotree history persistent
+vim.opt.undofile = true
+vim.opt.undodir = "~/.local/share/nvim/undo"
+vim.opt.undolevels = 1000
+vim.opt.undoreload = 10000
