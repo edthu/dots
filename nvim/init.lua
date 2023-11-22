98ff8a020131583fa2c246aeb2c199c7ad83126a
edthu <eetu.jalava@hotmail.fi>
2023-11-22 22:49:03 +0200

    tmux

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,6 +6,6 @@ require('lualine-config')
 vim.wo.relativenumber = true
 vim.wo.number = true
 vim.opt.ignorecase = true
-vim.bo.tabstop = 4
-vim.bo.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 
