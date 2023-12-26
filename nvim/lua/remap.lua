e5ab45d0b28768a9236041258907186472ecd061
edthu <eetu.jalava@hotmail.fi>
2023-12-26 14:58:26 +0200

    er ex

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>ee", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>er", vim.cmd.Ex)
 vim.keymap.set("i", '<A-j>', '<Esc>')
 vim.keymap.set("i", '<A-k>', '<Esc>')
 vim.keymap.set("v", '<A-k>', '<Esc>')
