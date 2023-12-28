9ffa9bc2c9b36a6b80b89d06206ac4445e7aa276
edthu <eetu.jalava@hotmail.fi>
2023-12-29 00:54:02 +0200

    bg_selector

--- Scripts/swaybg_selector.sh
+++ Scripts/swaybg_selector.sh
@@ -1,10 +1,12 @@
 #!/bin/zsh
-wallpaper_dir=$HOME/Wallpapers
+wallpaper_dir=$1
+monitor=$2
+option=$3
 entries=$(ls $wallpaper_dir)
  
 selected=$(printf '%s\n' $entries | wofi --show dmenu awk '{print tolower($1)}')
  
 case $selected in
         *.jpg|*.png|*.jpeg)
-		swaybg -i $wallpaper_dir/$selected -m fill
+		swaybg -o $monitor -i $wallpaper_dir/$selected -m $option ;;
 esac
