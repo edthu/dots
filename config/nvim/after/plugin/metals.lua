51d984b5b5b6992ee2dfe18011c6ed0fe0b1a595
edthu <eetu.jalava@hotmail.fi>
2024-03-01 21:15:47 +0200

    forgot some files

---
+++ config/nvim/after/plugin/metals.lua
@@ -0,0 +1,12 @@
+local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "scala", "sbt", "java" },
+	callback = function()
+	  require("metals").initialize_or_attach({})
+	end,
+	group = nvim_metals_group,
+})
+
+local metals_config = require("metals").bare_config()
+metals_config.init_options.statusBarProvider = "on"
---
+++ config/nvim/init.lua
@@ -0,0 +1,11 @@
+require('remap')
+require('plugins')
+require('colorscheme')
+require('lualine-config')
+
+vim.wo.relativenumber = true
+vim.wo.number = true
+vim.opt.ignorecase = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
